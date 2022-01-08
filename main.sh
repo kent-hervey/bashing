13ca7070365958adf8a2a9e921a209a53733d6a2
Kent Hervey <kent.hervey@employbridge.com>
2022-01-08 12:48:57 -0600

    added a case

--- main.sh
+++ main.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
 echo Hello world!
+
+echo "first";echo "second"
+
+Variable="Some String"
+
+echo $Variable 
+
+month=("Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec")
+echo ${month[1]}
+
+
+echo "Please talk to me ..."
+while :
+do
+  read INPUT_STRING
+  case $INPUT_STRING in
+	hello)
+		echo "Hello yourself!"
+		;;
+	bye)
+		echo "See you again!"
+		break
+		;;
+	*)
+		echo "Sorry, I don't understand"
+		;;
+  esac
+done
+echo 
+echo "That's all folks!"
